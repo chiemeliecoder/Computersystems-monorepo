8518887a4b87c79f91fcd09bb69dec3247dd3695
Chiemelie Ezeokeke <77821039+chiemeliecoder@users.noreply.github.com>
2023-01-24 04:48:59 -0500

    Update polybius.h
    updated header file of Polybius

--- A2/polybius.h
+++ A2/polybius.h
@@ -1,2 +1,22 @@
+#include <stdlib.h>
+
 #ifndef POLYBIUS_H
 #define POLYBIUS_H
+
+#endif
+
+//
+// Created by chiem on 1/23/2023.
+//
+
+
+static int  randomInt(int min, int max);
+static void printTable(char *table);
+static int  getTableIndex(char a, char *table);
+static char getTableChar(int index, char *table);
+
+char * generateTable();
+
+char * encrypt(char *table, const char *plaintext, enum TEXT_TYPE type);
+
+char * decrypt(char *table, const char *ciphertext);
