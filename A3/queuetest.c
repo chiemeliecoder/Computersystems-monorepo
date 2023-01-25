3ad32e88413d79d8a8fd6a6865dfe1be4ff1e1d0
Chiemelie Ezeokeke <77821039+chiemeliecoder@users.noreply.github.com>
2023-01-25 16:35:34 -0500

    Update queuetest.c

--- A3/queuetest.c
+++ A3/queuetest.c
@@ -1,5 +1,49 @@
 #include <stdio.h>
+#include <stdlib.h>
+#include<string.h>
+
+#include "queue.h"
+//
+// Created by chiem on 1/24/2023.
+//
+
+int unitTest1(int status){
+    printf("========queue unitTest1========\n");
+    queue_t* test1 = makeQueue(1);
+    enqueue(test1,1);
+    dequeue(test1);
+
+    //free_queue(test1);
+    return 1;
+}
+int unitTest2(int status){
+    printf("========queue unitTest2========\n");
+    queue_t*test7 = makeQueue(5);
+    enqueue(test7,1);
+    enqueue(test7,2);
+    enqueue(test7,3);
+    enqueue(test7,4);
+    enqueue(test7,5);
+    //free_queue(test7);
+    return 1;
+}
+int unitTest3(int status){}
+int unitTest4(int status){}
+
+int (*unitTests[])(int)={
+        unitTest1;
+        NULL;
+};
 
 int main(){
-	return 0;
+    unsigned int testsPassed = 0;
+    // List of Unit Tests to test your data structure
+    int counter =0;
+    while(unitTests[counter]!=NULL){
+        unitTests[counter](1);
+        counter++;
+    }
+
+    return 0;
 }
+
