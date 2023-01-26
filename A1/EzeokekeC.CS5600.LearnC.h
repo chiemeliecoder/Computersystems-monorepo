d7d44a695ff0a8474665031ac5662ba5c8ed148d
Chiemelie Ezeokeke <77821039+chiemeliecoder@users.noreply.github.com>
2023-01-26 01:30:28 -0500

    Update EzeokekeC.CS5600.LearnC.h

--- A1/EzeokekeC.CS5600.LearnC.h
+++ A1/EzeokekeC.CS5600.LearnC.h
@@ -2,6 +2,13 @@
 #define EzeokekeC.CS5600.LearnC_H
 #endif
 
+/*Ezeoekeke.c/ Assignment1
+ *
+ *Ezeokeke Chiemelie/ CS5600 /Northeastern University
+ *
+ *Spring2023/Jan192023
+ */
+
 
 int mpg2kml(int mpg);
 int mpg2lphm(int mpg);
