750ca035d88c2734a8764813aa66a3e520c9ff37
Chiemelie Ezeokeke <77821039+chiemeliecoder@users.noreply.github.com>
2023-01-26 17:34:08 -0500

    Update EzeokekeC.CS5600.LearnC.c

--- A1/EzeokekeC.CS5600.LearnC.c
+++ A1/EzeokekeC.CS5600.LearnC.c
@@ -9,38 +9,45 @@
  *Spring2023/Jan192023
  */
 
-int mpg2kml(int mpg){
-    char str[100];
+double mpg2kml(double* mpg){
+    char *str = (char *) &mpg;
+    double c = 0.425144;
+    double* convert = &c;
     if(mpg < 0 || mpg == NULL || mpg == str ){
         printf("Wrong input try numbers greater than 0: ");
         return -1;
     }
+    
+	double kpl;
+	kpl = ((*mpg) * (*convert))/1;
 
-    int kpl;
-    kpl = (mpg * 0.425144)/1;
-    return kpl;
+	return kpl;
 }
 
-int mpg2lphm(int mpg){
-    char str[100];
-    if(mpg < 0 || mpg == NULL || mpg == str){
+double mpg2lphm(double* mpg){
+    char *str = (char *) &mpg;
+    double c = 235.215;
+    double* convert = &c;
+    if(mpg < 0 || mpg == NULL || mpg == str ){
         printf("Wrong input try numbers greater than 0: ");
         return -1;
     }
-    int lpk;
-    lpk = (235.215)/mpg;
-    return lpk;
+	double lpk;
+	lpk = (*convert)/(*mpg);
+	return lpk;
 }
 
-int kml2mpg(int kpl){
-    char str[100];
+double kml2mpg(double* kpl){
+    char *str = (char *) &kpl;
+    double c = 2.35215;
+    double* convert = &c;
     if(kpl < 0 || kpl == NULL || kpl == str){
         printf("Wrong input try numbers greater than 0: ");
         return -1;
     }
-    int mpg;
-    mpg = (kpl * 2.35215)/1;
-    return mpg;
+	double mpg;
+	
+	mpg = ((*kpl) * (*convert))/1;
+	
+	return mpg;
 }
-
-
