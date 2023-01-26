a394bc5bfa94a5551f5499225297a2923c06c686
Chiemelie Ezeokeke <77821039+chiemeliecoder@users.noreply.github.com>
2023-01-25 21:11:58 -0500

    Update queue.h

--- A3/queue.h
+++ A3/queue.h
@@ -30,6 +30,7 @@ typedef struct node{
     struct node* next;
     struct node* prev;
     void *val;
+    process_t* p;
 }node_t;
 
 
@@ -42,12 +43,13 @@ struct queue{
     struct node* head; // Current 'head' of the queue
     struct node* tail; // The next free position in the queue
     int size; // How many total elements we currently have enqueued.
+    void *data;
 };
 
 typedef struct queue queue_t;
 
 queue_t *makeQueue();
-int enqueue(queue_t* queue, void* element);
+void enqueue(queue_t* queue, void* element);
 void* dequeue(queue_t* queue);
 process_t* dequeueProcess(queue_t* queue);
 int qsize(queue_t* queue);
