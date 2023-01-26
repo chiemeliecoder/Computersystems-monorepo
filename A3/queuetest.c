281e23b0d169e5b761a36f38af86cacd4b8ed2b2
Chiemelie Ezeokeke <77821039+chiemeliecoder@users.noreply.github.com>
2023-01-25 20:02:26 -0500

    Update queuetest.c

--- A3/queuetest.c
+++ A3/queuetest.c
@@ -9,45 +9,7 @@
 // Created by chiem on 1/24/2023.
 //
 
-// int unitTest1(int status){
-//     printf("========queue unitTest1========\n");
-//     queue_t* test1 = makeQueue();
-//     for(int i = 1; i <= 3; i++){
-//         struct queue *p = malloc(sizeof(*p));
-//         p->head = p->tail = i;
-//         enqueue(test1,p);
-//         dequeue(test1);
-//         printf("one node removed = (%d , %d), Size is %d\n", p->head, p->tail, test1->size);
-//     }
-    
 
-//     free(test1);
-//     return 1;
-// }
-// int unitTest2(int status){
-//     printf("========queue unitTest2========\n");
-//     queue_t* test2 = makeQueue();
-//     for(int i = 1; i <= 5; i++){
-//         struct queue *p = malloc(sizeof(*p));
-//         enqueue(test2,p);
-//         enqueue(test2,p);
-//         enqueue(test2,p);
-//         enqueue(test2,p);
-//         enqueue(test2,p);
-//         printf("one node added = (%d , %d), Size is %d\n", p->head, p->tail, test2->size);
-//     }
-    
-//     free(test2);
-//     return 1;
-// }
-// int unitTest3(int status){}
-// int unitTest4(int status){}
-
-// int (*unitTests[])(int)={
-//         unitTest1,
-//         unitTest2,
-//         NULL
-// };
 
 int main(void){
     queue_t* test1 = makeQueue();
@@ -71,7 +33,7 @@ int main(void){
     sprintf(p->name, "Process%d", i);
     enqueue(test1, p);
     printf("Enqueued %s .....\t", p->name);
-    printdata(test1->val);
+    printdata(test1->tail->val);
     }
 
     printf("\n");
