7f1a91fdf81ef3bb9bd73e5cd16f75fdc06384a1
Chiemelie Ezeokeke <77821039+chiemeliecoder@users.noreply.github.com>
2023-01-26 17:36:52 -0500

    Update main.c

--- A1/main.c
+++ A1/main.c
@@ -10,45 +10,51 @@
  */
 
 void unitTest1(){
-    int a = 20;
-    printf("miles per gallon to kilometer per litre: %d\n",mpg2kml(a));
-    printf("miles per gallon to litre per 100 kilometers: %d\n",mpg2lphm(a));
-    printf("kilometer per liter to miles per gallon: %d\n",kml2mpg(a));
+	double b = 20.425144;
+	double* a = &b;
+	printf("miles per gallon to kilometer per litre: %lf\n",mpg2kml(a));
+	printf("miles per gallon to litre per 100 kilometers: %lf\n",mpg2lphm(a));
+	printf("kilometer per liter to miles per gallon: %lf\n",kml2mpg(a));
 }
 
 void unitTest2(){
-    int a =50;
-    printf("miles per gallon to kilometer per litre: %d\n",mpg2kml(a));
-    printf("miles per gallon to litre per 100 kilometers: %d\n",mpg2lphm(a));
-    printf("kilometer per liter to miles per gallon: %d\n",kml2mpg(a));
+    double b =50.00;
+    double* a = &b;
+    printf("miles per gallon to kilometer per litre: %lf\n",mpg2kml(a));
+    printf("miles per gallon to litre per 100 kilometers: %lf\n",mpg2lphm(a));
+    printf("kilometer per liter to miles per gallon: %lf\n",kml2mpg(a));
 }
 
 void unitTest3(){
-    int a =-30;
-    printf("miles per gallon to kilometer per litre: %d\n",mpg2kml(a));
-    printf("miles per gallon to litre per 100 kilometers: %d\n",mpg2lphm(a));
-    printf("kilometer per liter to miles per gallon: %d\n",kml2mpg(a));
+    double b =-30.00;
+    double* a = &b;
+    printf("miles per gallon to kilometer per litre: %lf\n",mpg2kml(a));
+    printf("miles per gallon to litre per 100 kilometers: %lf\n",mpg2lphm(a));
+    printf("kilometer per liter to miles per gallon: %lf\n",kml2mpg(a));
 }
 
 void unitTest4(){
-    int a = NULL;
-    printf("miles per gallon to kilometer per litre: %d\n",mpg2kml(a));
-    printf("miles per gallon to litre per 100 kilometers: %d\n",mpg2lphm(a));
-    printf("kilometer per liter to miles per gallon: %d\n",kml2mpg(a));
+    void* a = NULL;
+    printf("miles per gallon to kilometer per litre: %lf\n",mpg2kml(a));
+    printf("miles per gallon to litre per 100 kilometers: %lf\n",mpg2lphm(a));
+    printf("kilometer per liter to miles per gallon: %lf\n",kml2mpg(a));
 }
 
 void unitTest5(){
-    int a = "hello";
-    printf("miles per gallon to kilometer per litre: %d\n",mpg2kml(a));
-    printf("miles per gallon to litre per 100 kilometers: %d\n",mpg2lphm(a));
-    printf("kilometer per liter to miles per gallon: %d\n",kml2mpg(a));
+    char str[100] = "hello";
+    char* b = &str;
+    double* a = &b;
+    printf("value: %f\n", a);
+    printf("miles per gallon to kilometer per litre: %lf\n",mpg2kml(a));
+    printf("miles per gallon to litre per 100 kilometers: %lf\n",mpg2lphm(a));
+    printf("kilometer per liter to miles per gallon: %lf\n",kml2mpg(a));
 }
 
 int main(){
     printf("=======================unitTest1=====================\n");
-    unitTest1();
-    printf("======================unitTest2======================\n");
-    unitTest2();
+	unitTest1();
+	printf("======================unitTest2======================\n");
+	unitTest2();
     printf("=======================unitTest3=====================\n");
     unitTest3();
     printf("=========================unitTest4===================\n");
