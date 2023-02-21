c54a5964bd937e019ba759469d0a834b2d9c9745
Chiemelie Ezeokeke <77821039+chiemeliecoder@users.noreply.github.com>
2023-02-21 00:41:15 -0500

    Update malloc.c

--- A7/malloc.c
+++ A7/malloc.c
@@ -2,12 +2,15 @@
 #include<stddef.h>
 #include "mymalloc.h"
 
-
+//Initializing the block of memory
 void initialize(){
  freeList->size=20000-sizeof(struct block);
+ //this a flag if free, it is set to 1 else 0
  freeList->free=1;
  freeList->next=NULL;
 }
+//to make sure that the memeory allocated fits properly and that the space for the requested allocated memory is not bigger or smaller but exact.
+// The arguments accepts a pointer to the metadata block whose size is more than required.A new metadata block pointer is created it points to  the location provided by passing a block of memory.
 
 void split(struct block *fitting_slot,size_t size){
  struct block *new=(void*)((void*)fitting_slot+size+sizeof(struct block));
@@ -18,6 +21,9 @@ void split(struct block *fitting_slot,size_t size){
  fitting_slot->free=0;
  fitting_slot->next=new;
 }
+
+//If this condition below is met, the memory exactly fits the required size. set free flag to 0 showing the memory is allocated and return the starting address of the block of memory allocated.
+//If this condition is met. The memory size is greater than what is required. call the frag function to cut the memory to make it fit to the required sizeand return the starting address of the block of memory allocated.
 
 
 void *MyMalloc(size_t noOfBytes){
@@ -46,12 +52,14 @@ void *MyMalloc(size_t noOfBytes){
   return result;
  }
  else{
+  //no sufficient memory to allocate and return a null pointer.
   result=NULL;
   printf("Sorry. No sufficient memory to allocate\n");
   return result;
  }
 }
 
+//function below helps solve external fragmentation by joining the consecutive free blocks by removing the metablocks lying in between
 void merge(){
  struct block *curr,*prev;
  curr=freeList;
@@ -65,6 +73,7 @@ void merge(){
  }
 }
 
+//deallocate memory
 void MyFree(void* ptr){
  if(((void*)mem<=ptr)&&(ptr<=(void*)(mem+20000))){
   struct block* curr=ptr;
