215e39a1542ccc1713148170e974d4b412fb99af
Chiemelie Ezeokeke <77821039+chiemeliecoder@users.noreply.github.com>
2023-02-21 01:05:27 -0500

    Update malloc.c

--- A7/malloc.c
+++ A7/malloc.c
@@ -2,84 +2,98 @@
 #include<stddef.h>
 #include "mymalloc.h"
 
+
 //Initializing the block of memory
-void initialize(){
- freeList->size=20000-sizeof(struct block);
- //this a flag if free, it is set to 1 else 0
- freeList->free=1;
+void init(){
+ freeList->size=HER-sizeof(struct block);
+ freeList->free=1;//this a flag if free, it is set to 1 else 0
  freeList->next=NULL;
 }
-//to make sure that the memeory allocated fits properly and that the space for the requested allocated memory is not bigger or smaller but exact.
-// The arguments accepts a pointer to the metadata block whose size is more than required.A new metadata block pointer is created it points to  the location provided by passing a block of memory.
 
-void split(struct block *fitting_slot,size_t size){
- struct block *new=(void*)((void*)fitting_slot+size+sizeof(struct block));
- new->size=(fitting_slot->size)-size-sizeof(struct block);
+//to make sure that the memeory allocated fits properly and that the space for the requested allocated memory is not bigger or smaller but exact.
+
+
+void frag(struct block *slot,size_t size){
+  // The arguments accepts a pointer to the metadata block whose size is more than required.A new metadata block pointer is created it points to  the location provided by passing a block of memory.
+ struct block *new=(void*)((void*)slot+size+sizeof(struct block));
+ new->size=(slot->size)-size-sizeof(struct block);
  new->free=1;
- new->next=fitting_slot->next;
- fitting_slot->size=size;
- fitting_slot->free=0;
- fitting_slot->next=new;
+ new->next=slot->next;
+ slot->size=size;
+ slot->free=0;
+ slot->next=new;
 }
 
-//If this condition below is met, the memory exactly fits the required size. set free flag to 0 showing the memory is allocated and return the starting address of the block of memory allocated.
-//If this condition is met. The memory size is greater than what is required. call the frag function to cut the memory to make it fit to the required sizeand return the starting address of the block of memory allocated.
 
-
-void *MyMalloc(size_t noOfBytes){
- struct block *curr,*prev;
- void *result;
+void *myMalloc(size_t noOfBytes){
+  //pointers to traverse through the freeList
+ struct block *current,*prev;
+  //the starting address of the allocated memory
+ void *res;
  if(!(freeList->size)){
-  initialize();
+  init();
   printf("Memory initialized\n");
  }
- curr=freeList;
- while((((curr->size)<noOfBytes)||((curr->free)==0))&&(curr->next!=NULL)){
-  prev=curr;
-  curr=curr->next;
+  //temporary pointer that points to the start of the metablock
+ current=freeList;
+  //If this condition below is met, the metablock cannot be used for the allocation
+ while((((current->size)<noOfBytes)||((current->free)==0))&&(current->next!=NULL)){
+  prev=current;
+  current=current->next;
   printf("One block checked\n");
  }
- if((curr->size)==noOfBytes){
-  curr->free=0;
-  result=(void*)(++curr);
+  //If this condition below is met, the memory exactly fits the required size. set free flag to 0 showing the memory is allocated and return the starting address of the block of memory allocated.
+ if((current->size)==noOfBytes){
+  current->free=0;
+  res=(void*)(++current);
   printf("Exact fitting block allocated\n");
-  return result;
+  return res;
  }
- else if((curr->size)>(noOfBytes+sizeof(struct block))){
-  split(curr,noOfBytes);
-  result=(void*)(++curr);
+ else if((current->size)>(noOfBytes+sizeof(struct block))){
+   //If this condition is met. The memory size is greater than what is required. call the frag function to cut the memory to make it fit to the required sizeand return the starting address of the block of memory allocated.
+  frag(current,noOfBytes);
+  res=(void*)(++current);
   printf("Fitting block allocated with a split\n");
-  return result;
+  return res;
  }
  else{
-  //no sufficient memory to allocate and return a null pointer.
-  result=NULL;
+   //no sufficient memory to allocate and return a null pointer.
+  res=NULL;
   printf("Sorry. No sufficient memory to allocate\n");
-  return result;
+  return res;
  }
 }
 
 //function below helps solve external fragmentation by joining the consecutive free blocks by removing the metablocks lying in between
-void merge(){
+
+void mergefrag(){
+  //pointers to traverse through the freeList
  struct block *curr,*prev;
+  //temporary pointer that points to the start of the metablock
  curr=freeList;
+  //The current next isnt null
  while((curr->next)!=NULL){
+   //the current and current next is free
   if((curr->free) && (curr->next->free)){
+    //current size
    curr->size+=(curr->next->size)+sizeof(struct block);
+    //current next is the next next
    curr->next=curr->next->next;
   }
+    //preivous becomes current
   prev=curr;
+    //current is current next 
   curr=curr->next;
  }
 }
 
 //deallocate memory
-void MyFree(void* ptr){
- if(((void*)mem<=ptr)&&(ptr<=(void*)(mem+20000))){
+void myFree(void* ptr){
+ if(((void*)mem<=ptr)&&(ptr<=(void*)(mem+HER))){
   struct block* curr=ptr;
   --curr;
   curr->free=1;
-  merge();
+  mergefrag();
  }
  else printf("Please provide a valid pointer allocated by MyMalloc\n");
 }
