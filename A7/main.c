15ab9f564c53ad3340887abe84c51ae22508c737
Chiemelie Ezeokeke <77821039+chiemeliecoder@users.noreply.github.com>
2023-02-21 01:13:51 -0500

    Update main.c

--- A7/main.c
+++ A7/main.c
@@ -4,15 +4,15 @@
 
 int main(){
   
-  int *p= (int)MyMalloc(100*sizeof(int));
-  char *q= (char)MyMalloc(250*sizeof(char));
-  int *r= (int)MyMalloc(1000*sizeof(int));
+  int *p= (int)myMalloc(100*sizeof(int));
+  char *q= (char)myMalloc(250*sizeof(char));
+  int *r= (int)myMalloc(1000*sizeof(int));
   
-  MyFree(p);
-  char *w= (char)MyMalloc(700);
+  myFree(p);
+  char *w= (char)myMalloc(700);
   
-  MyFree(r);
-  int *k= (int)MyMalloc(500*sizeof(int));
+  myFree(r);
+  int *k= (int)myMalloc(500*sizeof(int));
   printf("Allocation and deallocation is done successfully!");
   return 0;
  
