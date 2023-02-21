f1f4b73e20cfbf1a062a0cfabe060710802e347d
Chiemelie Ezeokeke <77821039+chiemeliecoder@users.noreply.github.com>
2023-02-21 00:34:36 -0500

    Update mymalloc.h

--- A7/mymalloc.h
+++ A7/mymalloc.h
@@ -9,14 +9,20 @@
 char mem[HER];
 
 struct block{
+ //the size of the block
  size_t size;
+ //This flag is used to know whether the block described by the metadata structure. It is set to 1 else 0
  int free;
+ //Pointer to the next metadata
  struct block *next; 
 
 };
 
+//Pointing to the main block of memory which is initially free no storage allocation yet
+
 struct block* freeList=(struct block*)mem;
 
+//Initializing the block of memory
 void initialize();
 void split(struct block *fitting_slot,size_t size);
 void *MyMalloc(size_t noOfBytes);
