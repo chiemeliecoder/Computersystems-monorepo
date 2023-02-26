6a540097b4f0563a83fe388ce3156c35380681ed
Chiemelie Ezeokeke <77821039+chiemeliecoder@users.noreply.github.com>
2023-02-26 05:16:35 -0500

    Create mymalloc.h

---
+++ A8/mymalloc.h
@@ -0,0 +1,26 @@
+#include<stdio.h>
+#include<stddef.h>
+#ifndef MYMALLOC_H
+#define MYMALLOC_H
+#endif
+
+#define PAGE_SIZE 2048
+
+char mem[PAGE_SIZE];
+
+struct block{
+ size_t size; //the size of the block
+ int free; //This flag is used to know whether the block described by the metadata structure. It is set to 1 else 0
+ struct block *next; //Pointer to the next metadata
+
+};
+
+//Pointing to the main block of memory which is initially free no storage allocation yet
+
+struct block* freeList=(struct block*)mem;
+
+void init();
+void frag(struct block *slot,size_t size);
+void *pm_malloc(size_t noOfBytes);
+void mergefrag();
+void pm_free(void* ptr);
