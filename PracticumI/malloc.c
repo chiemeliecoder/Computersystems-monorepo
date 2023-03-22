df165b2ab8dc4754f23520314c17d0edb4e4cbd9
Chiemelie Ezeokeke <77821039+chiemeliecoder@users.noreply.github.com>
2023-03-21 20:53:38 -0400

    Update malloc.c

--- PracticumI/malloc.c
+++ PracticumI/malloc.c
@@ -7,6 +7,7 @@
 #include <stdlib.h>
 #include <string.h>
 #include <unistd.h>
+#include <limits.h>
 
 pthread_mutex_t mutex = PTHREAD_MUTEX_INITIALIZER;
 
@@ -43,7 +44,7 @@ void init() {
 
 void frag(void *slot, size_t size) { 
   
-  // The arguments accepts a pointer to the metadata block whose size is more than required.A new metadata block pointer is created it points to  the location provided by passing a block of memory.
+  // The function accepts a pointer to the metadata block whose size is more than required.A new metadata block pointer is created it points to  the location provided by passing a block of memory.
   struct block *new = (struct block *)((char *)slot + size + sizeof(struct block));
   new->size = ((struct block *)slot)->size - size - sizeof(struct block);
   new->free = 1;
@@ -62,6 +63,24 @@ void frag(void *slot, size_t size) {
     return;
   }
 }
+
+// Function to allocate a physical page and map it to a virtual page 
+void *allocate_page(int page_num) { 
+  // Allocate physical memory 
+  void *page = mmap(NULL, PAGE_SIZE, PROT_READ | PROT_WRITE, MAP_PRIVATE | MAP_ANONYMOUS, -1, 0); 
+  if (page == MAP_FAILED) { 
+    return NULL; 
+  } 
+// Map virtual page to physical page 
+  pageing[page_num].status = USEDBIT; 
+  pageing[page_num].status |= DIRTYBIT; 
+// Mark page as dirty to force swap-out on first access 
+  if (mmap(page, PAGE_SIZE, PROT_READ | PROT_WRITE, MAP_FIXED | MAP_PRIVATE, swap_fd, page_num * PAGE_SIZE) == MAP_FAILED) { 
+    munmap(page, PAGE_SIZE); 
+    return NULL; 
+  } 
+  return page; 
+} 
 
 
 void *pm_malloc_lock(size_t noOfBytes) { 
@@ -88,8 +107,8 @@ void *pm_malloc_lock(size_t noOfBytes) {
   if (!current) {
     // No suitable block found, allocate more memory
     size_t size = (noOfBytes > PAGE_SIZE) ? noOfBytes : PAGE_SIZE;
-    void *mem = mmap(NULL, size, PROT_READ | PROT_WRITE, MAP_PRIVATE | MAP_ANONYMOUS, -1, 0);
-    if (mem == MAP_FAILED) {
+    void *res = mmap(NULL, size, PROT_READ | PROT_WRITE, MAP_PRIVATE | MAP_ANONYMOUS, -1, 0);
+    if (res == MAP_FAILED) {
         perror("Failed to allocate memory with mmap");
         pthread_mutex_unlock(&mutex);
         return NULL;
@@ -105,7 +124,16 @@ void *pm_malloc_lock(size_t noOfBytes) {
     // Update current pointer
     current = new_block;
   }
-  
+
+  for(int page_num = 0; page_num < NUM_PAGES; page_num++){ 
+
+    if(pageing[page_num].status == USEDBIT){ 
+      
+      allocate_page(page_num); 
+
+    } 
+
+  } 
   //If this condition below is met, the memory exactly fits the required size. set free flag to 0 showing the memory is allocated and return the starting address of the block of memory allocated.
  // pthread_mutex_unlock(&mutex);
  if((current->size)==noOfBytes){
@@ -113,6 +141,15 @@ void *pm_malloc_lock(size_t noOfBytes) {
   res=(void*)(++current);
   pthread_mutex_unlock(&mutex);
   printf("Exact fitting block allocated\n");
+  // Write to memory 
+
+  memset(res, 'A', noOfBytes); 
+
+  // Read from memory 
+
+  printf("Data at allocated memory location: %s\n", (char*)res); 
+
+     
   return res;
  }
  else if((current->size)>(noOfBytes+sizeof(struct block))){
@@ -170,6 +207,8 @@ void pm_free_lock(void *ptr) {
 }
 
 
+
+
 void read_page(int page_num, char *buf) {
   if (page_num < 0 || page_num >= NUM_PAGES) {
     fprintf(stderr, "Invalid page number: %d\n", page_num); 
@@ -217,7 +256,64 @@ int find_lru_page(PageTable *page_table) {
 }
 
 void access_page(PageTable *page_table, int page_id, int current_time) {
+  int lru_page_id = find_lru_page(page_table); 
+  
+  // Swap out the least recently used page  
+  
+  Page *lru_page = &page_table->pages[lru_page_id];  
+   // Update the last access time of the page
   page_table->pages[page_id].last_access_time = current_time; 
+   // Load the page into physical memory if it is not resident 
+  load_page(page_table, page_id, current_time); 
+  // Set the "used" bit for the page 
+  set_page_used(page_id, true);
+    // Check if there is enough free memory to load the page 
+  int free_page_id = -1; 
+  for (int i = 0; i < NUM_PAGES; i++) { 
+    if (!is_page_used(i)) { 
+      // Found a free page 
+      free_page_id = i; 
+      break; 
+    } 
+  } 
+
+  if (free_page_id == -1) { perror("Failed to find a page to swap out"); exit(1); } 
+   
+  // Check if the least recently used page is dirty  
+    
+  if (is_page_dirty(lru_page_id)) {  
+    
+    // Write the page to the swap file 
+  if (pwrite(swap_fd, physical_memory + (lru_page_id * PAGE_SIZE), PAGE_SIZE, page_table->pages[lru_page_id].disk_location) != PAGE_SIZE) {  
+    
+    perror("Failed to write page to swap file"); exit(1); } } // Mark the page as not resident in physical memory 
+  page_table->pages[lru_page_id].resident = false;  
+    
+    // Load the new page into physical memory  
+    
+  char buf[PAGE_SIZE];  
+  
+  if (pread(swap_fd, buf, PAGE_SIZE, page_table->pages[page_id].disk_location) != PAGE_SIZE) { perror("Failed to read page from swap file"); exit(1); }  
+  
+  memcpy(physical_memory + (free_page_id * PAGE_SIZE), buf, PAGE_SIZE);  
+    
+    // Update the page table  
+    
+  lru_page->id = page_id; 
+  
+  lru_page->resident = true; 
+  
+  lru_page->disk_location = page_table->pages[page_id].disk_location;  
+    
+  page_table->pages[page_id].resident = true;  
+  
+  page_table->pages[page_id].id = free_page_id;  
+  
+  page_table->pages[page_id].last_access_time = current_time; 
+    
+  // Mark the swapped out page as unused  
+    
+  set_page_used(lru_page_id, false); 
 }
 
 void load_page(PageTable *page_table, int page_id, int current_time) {
@@ -245,6 +341,23 @@ void load_page(PageTable *page_table, int page_id, int current_time) {
     access_page(page_table, lru_page, current_time); 
 
   } 
+
+  if (!page_table->pages[page_id].resident) { 
+    // Page is not resident in physical memory 
+    // Load it from swap file 
+    char buf[PAGE_SIZE]; 
+    if (pread(swap_fd, buf, PAGE_SIZE, page_table->pages[page_id].disk_location) != PAGE_SIZE) { 
+      perror("Failed to read page from swap file"); 
+      exit(1); 
+    } 
+    // Copy the page data to physical memory 
+    memcpy(physical_memory + (page_id * PAGE_SIZE), buf, PAGE_SIZE); 
+    // Update the page table 
+    page_table->pages[page_id].resident = true; 
+  } 
+  // Update the last access time of the page 
+  page_table->pages[page_id].last_access_time = current_time; 
+
 }
 
 void *access_tracker(void *arg){
@@ -304,3 +417,63 @@ void set_page_dirty(uint8_t page_num, bool dirty){
   
   // printf("1");
 }
+
+bool is_page_in_memory(PageTable *page_table, int page_id) { 
+
+  for (int i = 0; i < page_table->num_pages; i++) { 
+
+    if (page_table->pages[i].id == page_id && page_table->pages[i].last_access_time != -1) { 
+
+      return true; 
+
+    } 
+
+  } 
+
+  return false; 
+
+} 
+
+int replace_page(PageTable *page_table, int page_id) { 
+
+  int evicted_page_id = -1; 
+
+  int oldest_access_time = INT_MAX; 
+
+  for (int i = 0; i < page_table->num_pages; i++) { 
+
+    if (page_table->pages[i].last_access_time < oldest_access_time) { 
+
+      evicted_page_id = page_table->pages[i].id; 
+
+      oldest_access_time = page_table->pages[i].last_access_time; 
+
+    } 
+
+  } 
+
+  // write the contents of the evicted page to the swap file 
+
+  char *buf = physical_memory + evicted_page_id * PAGE_SIZE; 
+
+  write(swap_fd, buf, PAGE_SIZE); 
+
+  // update the Page struct with the new page id and access time 
+
+  for (int i = 0; i < page_table->num_pages; i++) { 
+
+    if (page_table->pages[i].id == evicted_page_id) { 
+
+      page_table->pages[i].id = page_id; 
+
+      page_table->pages[i].last_access_time = time(NULL); 
+
+      break; 
+
+    } 
+
+  } 
+
+  return evicted_page_id; 
+
+} 
