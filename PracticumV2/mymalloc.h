099304f43a35fd52cf3ca22f1eb596efb2022f1f
Chiemelie Ezeokeke <77821039+chiemeliecoder@users.noreply.github.com>
2023-03-31 00:48:24 -0400

    Create mymalloc.h

---
+++ PracticumV2/mymalloc.h
@@ -0,0 +1,112 @@
+#include <stdio.h>
+
+#include <stdlib.h>
+
+#include <string.h>
+
+#include <unistd.h>
+
+#include <stdint.h>
+
+#include <stdbool.h>
+
+#include <fcntl.h>
+
+#ifndef MYMALLOC_H
+#define MYMALLOC_H
+#endif
+
+#define NUM_THREADS 4
+
+
+// #define USEDBIT 0x80
+// #define DIRTYBIT 0x40
+
+
+
+#define NUM_PAGES 16
+
+
+// Define the size of the heap
+#define PAGE_SIZE 100777216
+
+// Static heap of memory
+extern char phys_mem[PAGE_SIZE];
+
+static char* heap = phys_mem;
+
+// Pointer to the next free block of memory
+static char* free_ptr = NULL;
+
+// Structure to store the metadata of each block of memory
+typedef struct block {
+    size_t size;
+    int free;
+    struct block* next;
+    void *data;
+} block_t;
+
+// Macro to get the metadata of a block of memory
+#define BLOCK(ptr) ((block_t*)((char*)(ptr) - sizeof(block_t)))
+
+extern int pageTab[NUM_PAGES];
+extern int dirty[NUM_PAGES];
+extern int pageNumber;
+
+
+
+
+
+/*
+    Defines a Virtual Memory Addressing table that can be
+    represented as either a TLB cache or Page Table
+*/
+typedef struct vmTable_t {
+    int *pageNumArr; // page number array
+    int *frameNumArr; // frame number array for this
+    int *entryAgeArr; // Age of each index
+    int length;
+    int pageFaultCount;
+    int tlbHitCount;
+    int tlbMissCount;
+} vmTable_t;
+
+
+
+// void pfree(void* ptr);
+
+// void* pmalloc(size_t size);
+void* thread_safe_malloc(size_t size);
+void thread_safe_free(void* ptr);
+void init_heap();
+void pm_put(void *ptr, void *data, size_t size);
+
+void pm_get(void *ptr, void *data, size_t size);
+
+
+// This function creates a new Virtual Memory Table for
+// Logical address referencing -- Can represent either the TLB or Page Table Cache
+vmTable_t* createVMtable(int length);
+
+// This function prints contents of the vmTable
+void displayTable(vmTable_t** tableToView);
+
+/* DECIDED NOT TO USE -- Too Complex to Implement, but keeping JIC I want to use/fix later
+// This function creates a physical memory sparse matrix
+dramMatrix* createDRAMmatrix(int frameCount, int frameSize);
+*/
+// This function frees dynamically allocated memory
+void freeVMtable(vmTable_t** table);
+
+// Accepts an int double pointer for creating simulated physical memory space
+int** dramAllocate(int frameCount, int blockSize);
+
+// Will free dram memory after usage
+void freeDRAM(int*** dblPtrArr, int frameCount);
+
+
+// 32-Bit masking function to extract page number
+int getPageNumber(int mask, int value, int shift);
+
+// 32-Bit masking function to extract page offset
+int getOffset(int mask, int value);
