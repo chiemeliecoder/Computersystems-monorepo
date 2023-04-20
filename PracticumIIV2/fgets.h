79a74b7f919d8c8b01261990ec4e6b19aa40e819
Chiemelie Ezeokeke <77821039+chiemeliecoder@users.noreply.github.com>
2023-04-19 20:21:52 -0400

    Update fgets.h

--- PracticumIIV2/fgets.h
+++ PracticumIIV2/fgets.h
@@ -25,13 +25,3 @@
 #define MAX_FILE_SIZE 8192
 #define MAX_FILENAME_LEN2 256
 #define MAX_COMMAND_LEN 256
-
-//Its a structure named thread_data that contains two fields: usb_device and file_path. It contains information about the USB device to read from and the file to write the data to.
-struct thread_data {
-  char* usb_device;
-  char* file_path;
-};
-
-
-//function read_from_usb that reads data from a USB device and writes it to a file.
-void* read_from_usb(void* arg);
